72817f56e7a1ab0feea724b375738d0a9fea669a
Marcelino Neto <mogt@gft.com>
2023-03-16 16:50:13 -0300

    feat: criacao de testes em API utilizando mocks

---
+++ Testando API/src/test/models/editora.test.js
@@ -0,0 +1,69 @@
+import Editora from "../../models/editora";
+import { describe, jest } from "@jest/globals";
+
+describe('Testando o modelo Editora', () => {
+    const objetoEditora = {
+        nome: 'CDC',
+        cidade: 'Sao Paulo',
+        email: 'c@c.com'
+    }
+
+    it('Deve instanciar uma nova editora', () => {
+        const editora = new Editora(objetoEditora)
+
+        expect(editora).toEqual(
+            expect.objectContaining(objetoEditora)
+        )
+    })
+
+    // faz chamada na api
+    it.skip('Deve salvar editora no BD', () => {
+        const editora = new Editora(objetoEditora)
+
+        editora.salvar().then((dados) => {
+            expect(dados.nome).toBe('CDC')
+        })
+    })
+
+    // faz chamada na api
+    it.skip('Deve salvar editora no BD usando a sintaxe moderna', async () => {
+        const editora = new Editora(objetoEditora)
+        const dados = await editora.salvar()
+
+        const retornado = await Editora.pegarPeloId(dados.id)
+
+        expect(retornado).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
+                ...objetoEditora,
+                created_at: expect.any(String),
+                updated_at: expect.any(String)
+            })
+        )
+    })
+
+    // mock
+    it('Deve simular uma chamadada de salvar no BD', () => {
+        const editora = new Editora(objetoEditora);
+
+        editora.salvar = jest.fn().mockReturnValue({
+            id: 10,
+            nome: 'CDC',
+            cidade: 'Sao Paulo',
+            email: 'c@c.com',
+            created_at: '2023-10-01',
+            updated_at: '2023-10-01'
+        })
+
+        const retorno = editora.salvar()
+
+        expect(retorno).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
+                ...objetoEditora,
+                created_at: expect.any(String),
+                updated_at: expect.any(String)
+            })
+        )
+    })
+})
