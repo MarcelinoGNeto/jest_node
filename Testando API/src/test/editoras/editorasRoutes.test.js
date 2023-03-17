4efe199f71f89f05fbc9f2b7d3b596f6fbeea204
Marcelino Neto <mogt@gft.com>
2023-03-17 11:02:17 -0300

    feat: teste de rotas - get(id), post e delete

--- Testando API/src/test/editoras/editorasRoutes.test.js
+++ Testando API/src/test/editoras/editorasRoutes.test.js
@@ -1,5 +1,6 @@
-import { afterEach, beforeEach } from "@jest/globals";
+import { afterEach, beforeEach, describe, expect } from "@jest/globals";
 import app from "../../app";
+import request from "supertest"
 
 let server;
 
@@ -12,4 +13,47 @@ beforeEach(() => {
 // Hook para derrubar a conexão/servidor após os testes
 afterEach(() => {
     server.close();
+})
+
+describe('GET em /editoras', () => {
+    it('Deve retornar uma lista de editoras', async () => {
+        const resposta = await request(app)
+            .get('/editoras')
+            .set('Accept', 'application/json') // setar paramentros/informações no header
+            .expect('content-type', /json/) // resposta da teste no header
+            .expect(200) // resposta da requisição da rota
+        
+        expect(resposta.body[0].email).toEqual('e@e.com') //verificando uma informação do corpo da requisição
+    })
+})
+
+let idResposta; // usar id dinamico nos testes da rota POST e utiliza-lo no teste da rota DELETE, evitando popular no BD
+describe('POST em /editoras', () => {
+    it('Deve adicionar uma nova editora', async () => {
+        const resposta = await request(app)
+            .post('/editoras')
+            .send({
+                nome: "CDC",
+                cidade: "São Paulo",
+                email: "c@c.com",
+            })
+            .expect(201); // espera a resposta com sucesso
+        idResposta = resposta.body.content.id;
+    })
+})
+
+describe('DELETE em /editoras/id', () => {
+    it('Deletar o recurso adicionado pela rota de POST', async () => {
+        await request(app)
+            .delete(`/editoras/${idResposta}`)
+            .expect(200) // espera a resposta com sucesso
+    })
+})
+
+describe('GET pega editora por id /editoras/id', () => {
+    it('deve retornar recurso selecionado', async () => {
+        await request(app)
+            .get(`/editoras/${idResposta}`)
+            .expect(200) // espera a resposta com sucesso
+    })
 })
